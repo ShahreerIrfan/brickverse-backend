a4531e10ccb2612bb1febb8d2a0e3bb969b97b2c
shahreerirfan <mdshahreerirfan@gmail.com>
2026-09-07 17:30:38 +0600

    Update seed_data with 10 products per category and add to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ echo "==> Waiting for database connection if needed..."
 echo "==> Running database migrations..."
 python manage.py migrate --noinput
 
+echo "==> Seeding initial catalog data..."
+python manage.py seed_data
+
 echo "==> Collecting static files..."
 python manage.py collectstatic --noinput
 
